76aba7032efc162560752053ff8a6796d0fde7c0
Bhasker Raju <saibhaskerraju@outlook.com>
2025-11-01 09:34:33 -0400

    adding more content in readme

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, FastAPI!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8070)
